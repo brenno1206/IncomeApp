e94837b38192e368f73adefde4f29f18c6fd3b62
Brenno Gomes <brennogbreda@gmail.com>
2025-10-14 18:48:24 -0300

    Initial Commit

---
+++ IncomeApp/IncomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeAppApp.swift
+//  IncomeApp
+//
+//  Created by Brenno Gomes Breda on 14/10/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
