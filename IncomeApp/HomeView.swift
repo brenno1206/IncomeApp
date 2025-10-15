29f2cc9ed0f1ee4b6149038d5fc3e1588a18c87a
Brenno Gomes <brennogbreda@gmail.com>
2025-10-15 09:48:23 -0300

    Formatting Transaction view

--- IncomeApp/HomeView.swift
+++ IncomeApp/HomeView.swift
@@ -10,23 +10,46 @@ import SwiftUI
 struct HomeView: View {
     
     @State private var transactions: [Transaction] = [
-        Transaction(title: "Apple", type: .expense, amount: 120.00, date: Date())
+        Transaction(title: "Apple", type: .income, amount: 120.00, date: Date())
     ]
     
     var body: some View {
         VStack {
             List {
                 ForEach(transactions) { transaction in
-                    HStack {
-                        Image(systemName: transaction.type == .income ? "arrow.up.forward" : "arrow.down.forward" )
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Text(transaction.displayDate)
+                                .font(.system(size: 14))
+                            Spacer()
+                        }
+                        .padding(.vertical, 5)
+                        .background(Color.lightGrayShade.opacity(0.5))
+                        .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                        HStack {
+                            Image(systemName: transaction.type == .income ? "arrow.up.forward" : "arrow.down.forward" )
+                                .font(.system(size: 16, weight: .bold))
+                                .foregroundStyle(transaction.type == .income ? .green : .red)
+                            VStack(alignment: .leading, spacing: 5) {
+                                HStack {
+                                    Text(transaction.title)
+                                        .font(.system(size: 15, weight: .bold))
+                                    Spacer()
+                                    Text(transaction.displayAmount)
+                                        .font(.system(size: 15, weight: .bold))
+                                }
+                                Text("Completed")
+                                    .font(.system(size: 14))
+                            }
+                        }
                     }
                 }
             }
         }
-        .padding()
     }
 }
 
-#Preview {
-    HomeView()
-}
+//#Preview {
+//    HomeView()
+//}
